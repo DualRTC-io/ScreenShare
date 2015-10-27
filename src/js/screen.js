c0de79f8e7ca27954ea66dbfecb0a3f9cfdcd1da
sungjungkim <ajax0615@gmail.com>
2015-10-27 23:52:22 +0900

    Change extensionId

--- src/js/screen.js
+++ src/js/screen.js
@@ -2,7 +2,7 @@
 var apiKey    = '45369152';
 var sessionId = '1_MX40NTM2OTE1Mn5-MTQ0NDM5MDc5NDExOH5PRjNMK2ZjRktaS29mTTE1SWpnc1gzb3V-UH4';
 var token     = 'T1==cGFydG5lcl9pZD00NTM2OTE1MiZzaWc9ZjU1YjI4YzNiNTMzOGExOWMyOTI4M2YxYjI0YmVjYmU4YjQyM2UyYjpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTFfTVg0ME5UTTJPVEUxTW41LU1UUTBORE01TURjNU5ERXhPSDVQUmpOTUsyWmpSa3RhUzI5bVRURTFTV3BuYzFnemIzVi1VSDQmY3JlYXRlX3RpbWU9MTQ0NDM5MDgxOCZub25jZT0wLjUxODAwNDg2ODU3NTI3NiZleHBpcmVfdGltZT0xNDQ0NDc3MTY0JmNvbm5lY3Rpb25fZGF0YT0=';
-var extensionId = 'bibaekpncanjnbnkpichhjhmgkemiflb';
+var extensionId = 'kdgjponegkkhkigjlknapimncipajbpi';
 var session = OT.initSession(apiKey, sessionId);
 
 function initBody() {
@@ -11,7 +11,7 @@ function initBody() {
 }
 
 function initShareScreen() {
-    var button = document.getElementById("shareScreen");
+    var button = documshareScreennt.getElementById("shareScreen");
     button.addEventListener('click', function (event) {
         screenshare();
     });
@@ -50,7 +50,7 @@ function screenshare() {
             // var screenContainerElement = document.createElement('div');
             var screenContainerElement = document.getElementById("shareScreen");
             var screenSharingPublisher = OT.initPublisher(screenContainerElement,
-                { videoSource : 'screen',width:1280,height:720 },
+                { videoSource : 'screen', width:1280, height:720 },
                 function(error) {
                     if (error) {
                         alert('Something went wrong: ' + error.message);
